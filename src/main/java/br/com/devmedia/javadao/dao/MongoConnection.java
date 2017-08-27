c1ce6c75242ed854c00b331da284994b920e36fd
Lildemar Matias dos Santos <lildemar@gmail.com>
2017-08-27 18:13:19 -0300

    Projeto prático: Java DAO - Projeto e Classes - Curso de MongoDB - Aula 36

---
+++ src/main/java/br/com/devmedia/javadao/dao/MongoConnection.java
@@ -0,0 +1,42 @@
+package br.com.devmedia.javadao.dao;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+public class MongoConnection {
+
+	private static final String HOST = "localhost";
+	private static final int PORT = 27017;
+	private static final String DB_NAME = "simple-mongodb";
+
+	private static MongoConnection uniqInstance;
+	private static int mongoInstance = 1;
+
+	private Mongo mongo;
+	private DB db;
+
+	private MongoConnection() {
+	}
+
+	public static synchronized MongoConnection getInstance() {
+		if (uniqInstance == null) {
+			uniqInstance = new MongoConnection();
+		}
+		return uniqInstance;
+	}
+
+	@SuppressWarnings("deprecation")
+	public DB getDB() {
+		if (mongo == null) {
+			try {
+				mongo = new Mongo(HOST, PORT);
+				db = mongo.getDB(DB_NAME);
+
+				System.out.println("Mongo instance equals :> " + mongoInstance++);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return db;
+	}
+}
---
+++ src/main/java/br/com/devmedia/javadao/entity/Phone.java
@@ -0,0 +1,40 @@
+package br.com.devmedia.javadao.entity;
+
+public class Phone {
+
+	private String phoneNumber;
+	private String mobileNumber;
+
+	public Phone() {
+		super();
+	}
+
+	public Phone(String phoneNumber, String mobileNumber) {
+		this.phoneNumber = phoneNumber;
+		this.mobileNumber = mobileNumber;
+	}      
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                '}';
+    }
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(String p) {
+		this.phoneNumber = p;
+	}
+
+	public String getMobileNumber() {
+		return this.mobileNumber;
+	}
+
+	public void setMobileNumber(String m) {
+		this.mobileNumber = m;
+	}
+}
